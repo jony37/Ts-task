f2036c31fb71b04f83d55e4d74134751ad3af28c
jony <ravshanovjony7@gmail.com>
2025-05-26 20:52:41 +0500

    6 dars tupe

--- sami.js
+++ sami.js
@@ -10,3 +10,21 @@
 // Union types
 // const mix: (number | string | boolean)[] = ["Jahon", 23, true];
 // console.log(mix);
+///////////////// 6 dars Tupe ////////////////////
+// let joha: [number, string | number] = [1, 23];
+// console.log("1", joha);
+// joha = [21, "Jahon"];
+// console.log("2", joha);
+// joha = [32, 43];
+// console.log("3", joha);
+// // tupe with function
+// function getinfo(): [string, number | string] {
+//   return ["Helo", 12];
+// }
+// const logger = getinfo()
+// console.log(logger[0]);
+// console.log(logger[1]);
+// // tupe with destruktizatsiya
+// const [fullName, age] = logger;
+// console.log(fullName);
+// console.log(age);
